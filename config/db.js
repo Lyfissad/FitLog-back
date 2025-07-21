1fee0a1db79b7de62023224bc2242e12453c4a95
Muazzin <muazzin2009@gmail.com>
2025-07-21 14:38:57 +0500

    dns issue

--- config/db.js
+++ config/db.js
@@ -9,14 +9,10 @@ dotenv.config();
 
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: 5432,
-    ssl:{
-        rejectUnauthorized: false
-    }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // required for Supabase
+  },
 });
 
 pool.connect()
