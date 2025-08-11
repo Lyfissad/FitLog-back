7e679e7bb044085e86acdc478b5f399345f2976a
Muazzin <muazzin2009@gmail.com>
2025-08-12 01:26:02 +0500

    profile fix

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.get("/profile", Auth, async (req, res) => {
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }
-    res.status(200).json({ message: "Profile loaded", user });
+    res.status(200).json({ message: "Profile loaded", user: req.user });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
